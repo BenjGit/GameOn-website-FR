7319cd16885ab7da7901669844a8190efbd007bb
Courtin <benjidu490@gmail.com>
2023-05-15 17:59:35 +0200

    verification des espaces pour le nom et prenom + message d'erreur a gauche + modal de confirmation fidele maquette + message d'erreurs afficher pour toutes les erreurs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,13 +13,15 @@ const submitBtn = document.querySelector(".btn-submit");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelectorAll(".close"); // add close btn
 const formData = document.querySelectorAll(".formData");
-const confirmation = document.querySelector('[data-success=confirmation]');
+const modalBody = document.querySelector('.modal-body');
+const confirmation = document.getElementById('confirmation');
+const form = document.querySelector('form');
 //launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 //close modal event
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
-submitBtn.addEventListener("click", (event) => validate(event));
+submitBtn.addEventListener("click", validate);
 
 function launchModal() {
   modalbg.style.display = "block";
@@ -28,6 +30,11 @@ function launchModal() {
 function closeModal(){
   modalbg.style.display = "none";
   confirmation.style.display = "none";
+  //réinitialiser le bouton fermer en bouton c'est parti pour pouvoir envoyer le formulaire
+  form.style.visibility = "visible";
+  submitBtn.value = "C'est parti";
+  submitBtn.removeEventListener("click", closeModal);
+  submitBtn.addEventListener("click", validate);
 }
 /**
  * 
@@ -51,7 +58,6 @@ function getAge(birthdate){
   return age;
 }
 
-
 /**
  * permet d'afficher un message d'erreur
  * @param {string} $errorMessage
@@ -69,11 +75,12 @@ function displayError($errorMessage , $divErrorName){
  * @returns //
  */
 function validate(event) {
-   
-    const form = document.querySelector('form');
+  
     const firstName = document.getElementById("first").value;
+    const firstNameSpace = firstName.split(' ');
     const firstNameError = document.getElementById('firstError');
     const lastName = document.getElementById('last').value;
+    const lastNameSpace = lastName.split(' ');
     const lastNameError = document.getElementById('lastError');
     const birthdate = document.getElementById('birthdate').value;
     const birthdateError = document.getElementById('birthdateError');
@@ -86,6 +93,8 @@ function validate(event) {
     const locationError = document.getElementById('locationError');
     const termsAndConditions = document.getElementById('checkbox1');
     const termsError = document.getElementById('termsError');
+
+    let isValid = true;
     
     // Réinitialiser l'affichage des messages d'erreur
     firstNameError.setAttribute("data-error-visible", "false");
@@ -96,33 +105,33 @@ function validate(event) {
     locationError.setAttribute("data-error-visible", "false");
     termsError.setAttribute("data-error-visible", "false");
 
-    if(firstName.length < 2 || firstName.length >50){
-      displayError('Veuillez entrer 2 caractères ou plus pour le prénom.', firstNameError);
+    if(firstName.length < 2 || firstName.length >50 || firstNameSpace.length > 2){
+      displayError("Veuillez entrer 2 caractères ou plus pour le prénom", firstNameError);
       event.preventDefault();//empêcher l'envoi car il y a une erreur
-      return; //sortir de la fonction pour empêcher le reste du code d'être éxécuté
+      isValid = false;
     }
 
-    if(lastName.length < 2 || lastName.length > 50){
-      displayError('Veuillez entrer 2 caractères ou plus pour le nom.', lastNameError);
+    if(lastName.length < 2 || lastName.length > 50 || lastNameSpace.length > 2){
+      displayError("Veuillez entrer 2 caractères ou plus pour le nom", lastNameError);
       event.preventDefault();
-      return;
+      isValid = false;
     }
 
     if(!emailRegex.test(email) || email ==""){
       displayError('Veuillez entrer une adresse email valide', emailError);
       event.preventDefault();
-      return
+      isValid = false;
     }
 
     if(!birthdate){
       displayError('Veuillez entrer votre date de naissance', birthdateError);
       event.preventDefault();
-      return
+      isValid = false;
     }
     else if(getAge(new Date(birthdate)) < 18 ){
       displayError('Vous devez être majeur pour vous inscrire', birthdateError);
       event.preventDefault();
-      return
+      isValid = false;
     }
      
     if(nbTournament.value === '')
@@ -133,7 +142,7 @@ function validate(event) {
     else if(nbTournament.value < 0 || nbTournament.value > 99){
       displayError("Veuillez entrer un nombre entre 0 et 99", nbTournamentError);
       event.preventDefault();
-      return
+      isValid = false;
     }
 
     let numChecked = 0;
@@ -146,20 +155,29 @@ function validate(event) {
     if (numChecked !==1) {
       displayError('Veuillez choisir une option', locationError);
       event.preventDefault();
-      return
+      isValid = false;
     }
     
     if(!termsAndConditions.checked){
       displayError('Vous devez acceptez les termes et conditions.', termsError);
       event.preventDefault();
-      return
+      isValid = false;
     } 
 
-    if(form.checkValidity()){
+    if (!isValid) {
+      event.preventDefault();
+    } else {
       form.reset();
       modalbg.style.display = "none";
-      confirmation.style.display = "block";
+      form.style.visibility= "hidden"// masquer le formulaire pour garder sa forme 
+      confirmation.style.display = "flex";//afficher le message de confirmation au milieu
+      submitBtn.value = "Fermer";// changer le nom du bouton submit "c'est parti" en "Fermer"
+      submitBtn.removeEventListener("click", validate);//enlever la fonction validate au bouton
+      submitBtn.addEventListener("click", closeModal);// ajouter la fonction closemodal au bouton fermer
       event.preventDefault();
+      setTimeout(() => {
+        launchModal();
+      }, 200);// ajouter un léger delai avant d'afficher le message de validation
     }
     
 }
